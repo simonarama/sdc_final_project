9253c71118079021d9a373a568b29a557221c218
Feng Xiao <feng.xiao@bmw-carit.de>
2018-03-29 13:12:53 +0200

    fix the timeout issue in styx server

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -65,4 +65,4 @@ if __name__ == '__main__':
     app = socketio.Middleware(sio, app)
 
     # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+    eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 4567)), app)
