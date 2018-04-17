14d22d251dfa626ccce1384efafeb735aec5261f
Feng Xiao <xiaofeng.ustc@gmail.com>
2018-04-17 09:13:26 +0200

    change to use the model trained with annotated simulator data

--- ros/src/tl_detector/settings.py
+++ ros/src/tl_detector/settings.py
@@ -3,10 +3,8 @@ place to kee the path to resources used in this module
 """
 
 # path to the check point frozen inference binary proto buf file
-traffic_light_inference_file = '../../data/models/ssd_mobilenet_v2_traffic_light_inference_graph.pb'
-
+traffic_light_inference_file = '../../data/models/ssd_mobilenet_v2_tl_sim_3_classes.pb'
 # path to the definition of labels, names of classes
-traffic_light_labels_file = '../../data/small_traffic_label_map_4.pbtxt'
-
+traffic_light_labels_file = '../../data/annotated_label_map_3.pbtxt'
 # number of classes for traffic light
-traffic_light_num_classes = 4
+traffic_light_num_classes = 3
