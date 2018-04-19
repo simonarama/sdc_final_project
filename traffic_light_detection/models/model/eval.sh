9acc89a6438f014e73cb0818c59b358b86963449
Feng Xiao <xiaofeng.ustc@gmail.com>
2018-04-19 10:25:48 +0200

    update the scripts for real traffic light classificaiton training

--- traffic_light_detection/models/model/eval.sh
+++ traffic_light_detection/models/model/eval.sh
@@ -2,7 +2,8 @@ python eval.py \
    --logtostderr \
    --checkpoint_dir=train \
    --eval_dir=eval \
-   --pipeline_config_path=ssd_mobilenet_v2_coco_3_classes_annotated_sim.config
+   --pipeline_config_path=ssd_mobilenet_v2_coco_3_classes_annotated_real.config
+   #--pipeline_config_path=ssd_mobilenet_v2_coco_3_classes_annotated_sim.config
    #--pipeline_config_path=faster_rcnn_resnet50_coco_4_classes.config
    #--pipeline_config_path=faster_rcnn_inception_v2_coco.config
    #--pipeline_config_path=ssd_mobilenet_v2_coco_4_classes.config
--- traffic_light_detection/models/model/export_inference_graph.sh
+++ traffic_light_detection/models/model/export_inference_graph.sh
@@ -1,7 +1,8 @@
 python export_inference_graph.py \
     --input_type image_tensor \
-    --trained_checkpoint_prefix ./train/model.ckpt-21746 \
+    --trained_checkpoint_prefix ./train/model.ckpt-20522 \
     --output_directory ./result \
-    --pipeline_config_path ssd_mobilenet_v2_coco_3_classes_annotated_sim.config
+    --pipeline_config_path ssd_mobilenet_v2_coco_3_classes_annotated_real.config
+    #--pipeline_config_path ssd_mobilenet_v2_coco_3_classes_annotated_sim.config
     #--pipeline_config_path faster_rcnn_resnet50_coco_4_classes.config
     #--pipeline_config_path ssd_mobilenet_v2_coco_4_classes.config
